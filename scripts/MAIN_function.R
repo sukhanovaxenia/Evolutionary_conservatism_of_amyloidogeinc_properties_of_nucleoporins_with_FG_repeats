a35a8d19ceb935bbd78fcc18f19a8578325708ec
sukhanovaxenia <60236068+sukhanovaxenia@users.noreply.github.com>
2021-05-28 21:34:19 +0300

    Update MAIN_function.R

--- scripts/MAIN_function.R
+++ scripts/MAIN_function.R
@@ -15,14 +15,15 @@ library(stringr)
 
 #Function to automatically get 10 different subsets of multiple alignments with MUSCLE algorithm:
 muscle_refine<-function(fastaFile =dir(pattern = '*opisto_ortho.fa'), file=dir(pattern = 'tax_report.txt'), tree = dir(pattern = '*_tax_ID.tsv'), filtered= 'filt.fa', filtered_noname = 'filt_noname.fa', outputpath = 'ali_R.fa', outputpath_noname = 'ali_noname_R.fa'){
-  #fastaFile - фаста с набором последовательностей для белка до замены ID
-  #file - файл с полученным с помощью скрипта Лаврентия таксономическим списком
-  #tree - файл с полной таблицей таксономии
-  #input (optional) - название файла для сохранения фасты с замененными на названия видов ID
-  #filtered - название файла для сохранения отфильтрованной фасты и последующей её подгрузкой (ОБАЗЕТЕЛЕН)
-  #outputpath -название для сохранения файла выравнивания
+  #fastaFile - default multifasta with orthologs sequences (*_opisto_ortho.fa)
+  #file - result_table_all.tsv file gained by Danilov Lavrentyii's python script
+  #tree - full taxonomy table
+  #input (optional) - optional multifile file where sequence IDs are replaced with species' names
+  #filtered - filename for filtered multiple fasta (OBLIGATORY)
+  #outputpath - file name for filtered multiple alignment with sequence labeles 'species's name.sequence ID'
+  #outputpath_noname - file name for filtered multiple alignment with sequence labeles 'sequence ID'
   
-  #Загружаем набор последовательностей для замены ID на названия видов
+  #Load multifasta file for replacing IDs on 'species's name.sequence ID':
   fastaFile <- readAAStringSet(fastaFile)
   seq_name = names(fastaFile)
   sequence = paste(fastaFile)
@@ -30,53 +31,51 @@ muscle_refine<-function(fastaFile =dir(pattern = '*opisto_ortho.fa'), file=dir(p
   df$seq_name<-as.character(df$seq_name)
   df<-separate(df, seq_name, c('seq_ID', 'seq_name'),sep='[.]', extra='merge',fill='right')
   df<-df[order(df$seq_ID),]
-  #загружаем полученный tax_report и оставляем только колонки taxid и taxname
-  ##ATTENTION: предварительно перед загрузкой надо удалить \t в .txt файле, так как это помешает соединить таблицы
+  #Import tax_report.txt file
+  ##ATTENTION: all \t should be removed from tax_report.txt file before import, otherwise it would cancel merging
   tax_rep<-read.csv(file, header=T, sep='|')
   tax_rep$code<-NULL
   tax_rep$primary.taxid<-NULL
   tax_rep$taxname<-as.character(tax_rep$taxname)
   tax_rep<-tax_rep[order(match(tax_rep$taxid,df$seq_ID)),]
-  #Добавляем в таблицу с последовательностями названия видов, соотнося id
+  #Add species' names to the df with sequences merging by sequences' IDs:
   df<-merge(df, tax_rep,by.x='seq_ID', by.y = 'taxid')
   df<-df[!duplicated(df$seq_name),]
   names(df)[4]<-'species'
   print(str(df))
-  #Создаем второй df для дальнейшей работы:
+  #СCreating the second the same df for the following work:
   df2<-data.frame(name = df$species, ID = df$seq_name, sequence = df$sequence)
   df2$name<-as.character(df2$name)
-  #Загружаем файл полной таксономии: царство, фила, класс, семейство, род, вид
+  #Load the file with full taxonomy: kingdom, phylum, class, order, species
   tree<-read.table(tree, sep='\t', header = T)
-  #Считаем частоту встречаемость каждого класса - количество последовательностей в классе
+  #Count frequencies of each class - the number of sequences in each of classes:
   freq<-as.data.frame(table(tree$class))
-  #Добавляем информацию об объеме класса в полную таблицу таксономии
+  #Add tha classes' frequencies into the full taxonomy table:
   new<-merge(tree, freq, by.x = 'class', by.y = 'Var1')
-  #new$species<-paste(new$species, new$seq_ID, sep='|')
-  #new<-new[,-8]
-  #Объединяем таксономию с последовательностями
+  #Merge taxonomy with sequences:
   full<-merge(new, df2[,c(1,3)], by.x = 'species', by.y = 'name')
-  #Убираем потворяющиеся значения, возникшие при объединении
+  #Remove duplications appeared after merging:
   full[,-c(7,8)]<-lapply(full[,-c(7,8)], factor)
   full2<-full[unique(full$seq_ID),]
   full2[,c(1,2,3,4,5,6)]<-lapply(full2[,c(1,2,3,4,5,6)], factor)
-  #Сабсетим только классы, в которых от 3 до 10 (включительно) последовательностей
+  #Subsets classes with more than 3 and less than 10 sequences:
   full_3_10<-full2 %>% group_by(class) %>% filter(Freq >3 & Freq <=10)
-  #Сабсетим классы, в которых более 10 последовательносетй:
+  #Subset only classes with more than 10 sequences:
   full_out_10<- full2 %>% group_by(class) %>% filter(Freq >10)
-  #Выбираем случайно 10 последовательностей для классов большими наборами (>10):
+  #Choose randomly 10 sequences for classes's subsets with more than 10 sequences:
   full_out_10<-ddply(full_out_10, .(class), function(x) x[sample(nrow(x),10, replace=T),])
-  #Получаем итоговый df
+  #Get the final df of filtered fasta
   full_ed<-rbind.fill(full_3_10, full_out_10)
   full_ed<-full_ed[duplicated(full_ed$seq_ID)==F,]
-  #Готовим переменные для создания фасты с последующей ее подгрузкой:
+  #Prepare values for filtered multifasta export:
   sequences<-as.list(as.character(full_ed$sequence))
   name<-as.list(as.character(paste(full_ed$species, full_ed$seq_ID, sep=".")))
   write.fasta(sequences,name,file.out = filtered,open='w',as.string = T)
-  #Загружаем ранее созданную фасту для выравнивания
+  #Loading previously filtered multifasta
   ali<-readAAStringSet(filepath = filtered, format = 'fasta')
-  #Сырое выравнивание
+  #Draft muscle alignment
   draft_muscle<-muscle(ali)
-  #Refine выравнивание
+  #Refine alignment
   draft_muscle<-AAStringSet(draft_muscle)
   muscle_refine<-muscle(draft_muscle, refine = T)
   #Экспот результата
@@ -140,21 +139,17 @@ aa_stat<-function(fasta = dir(pattern = 'ali_R.fa'), stat = 'ali_stat.tsv', plot
   aa_pos<-c(aa_sum2$position)
   aa_pos<-gsub('V', '',aa_pos)
   aa_sum2$position<-as.numeric(aa_pos)
-  #pdf(plot)
   ggplot(aa_sum2)+geom_tile(aes(x = position, y = aa, fill = fraction))+theme_bw()+scale_fill_viridis_c(name = "",limits = c(0.0,1.0),oob=squish)
-  #dev.off()
-  ##pdf(plot) and dev.off() as argument 'plot' are for the case is exporting by a fuction is preferable (honestly, I got empty pdf files after function call)
 }
 
 #The function to get cumulative scores for each repetition:
-CS_subset<-function(alignment =dir(pattern  ='ali_noname_R.fa'), cumscore = dir(pattern = 'CumScores.tsv'), taxonomy =dir(pattern = 'result_table_all.tsv'), id = dir(pattern='*_nog_orthologs.txt'), cs_output ='CS_subset.tsv', PA_output ='Report_table.tsv', aa_stat ='AA_plot.pdf',aa_stat_wNA='AA_plot_wNA.pdf', summary = 'Summary_stat.tsv', cs_stat_comp = 'CS_comp_plot.pdf', summary_wNA = 'Summary_wNA.tsv'){
+CS_subset<-function(alignment =dir(pattern  ='ali_noname_R.fa'), cumscore = dir(pattern = 'CumScores.tsv'), taxonomy =dir(pattern = 'result_table_all.tsv'), id = dir(pattern='*_nog_orthologs.txt'), cs_output ='CS_subset.tsv', aa_stat ='AA_plot.pdf',aa_stat_wNA='AA_plot_wNA.pdf', summary = 'Summary_stat.tsv', cs_stat_comp = 'CS_comp_plot.pdf', summary_wNA = 'Summary_wNA.tsv'){
   #This function count noticed parametres
   ## alignment - all previously gotten alignment for each repetition are taken by this function;
   ## cumsore - gotten by ArchCandy umulative scores for each sequence in the default orthologs' dataset (one for all repetitions)
   ## taxonomy - right taxonomy gotten from NCBI db for the orthologs' subset (one for all repetitions)
   ## id - the list of ID's for the analyzing protein (one for all repetitions)
   ## cs_output - cumulative scores for each repetitions;
-  ## PA_output - number of amyloidogenic sequences for big taxonomy groups of each repetition
   ## aa_stat - plots of analyzed parametres for each position in each repetition without NAs
   ## aa_stat_wNA - plots of analyzed parametres for each position in each repetition with NAs
   ## summary - summary of analyzed parametres for subsets withot NAs
@@ -180,19 +175,14 @@ CS_subset<-function(alignment =dir(pattern  ='ali_noname_R.fa'), cumscore = dir(
   
   tax_table2 <- merge(tax_table, id_table[,c('ID', 'species', 'spID')], by.x = 'species', by.y = 'species', all.y = T)
   tax_table2 <-tax_table2[order(tax_table2$kingdom, tax_table2$phylum, tax_table2$class, tax_table2$order),]
-  #tax_table2[!complete.cases(tax_table2),]
   
   
   CumScore_w_tax <- merge(y = CumScore, x = tax_table2, by.y = 'V1', by.x = 'ID',all.y = T)
   cumscor_test<-CumScore_w_tax%>% separate(ID, into=c('ID_sp', 'ID_seq'), sep = '\\.', extra = 'merge')
   cumscor<-cumscor_test[cumscor_test$ID_seq %in% names(ali2),]
   cumscor<-cumscor[match(names(ali2), cumscor$ID_seq),]
-  #cumscor2<-apply(cumscor_test[cumscor_test$ID_seq %in% names(ali2),9:ncol(cumscor_test)],1, function(x) x[!is.na(x)])
-  
-  
-  
-  
-  
+
+   
   rearrangeCumScore <- function(CumScore_w_NA, alignment){
     CumScore1 = CumScore_w_NA[!(is.na(CumScore_w_NA))]
     output = numeric(length(alignment))
@@ -215,52 +205,12 @@ CS_subset<-function(alignment =dir(pattern  ='ali_noname_R.fa'), cumscore = dir(
   }
   cumscor_out<-data.frame(ID = cumscor$ID_seq, species = cumscor$species, test_table)
   rownames(cumscor_out)<-rownames(cumscor)
-  write.table(cumscor_out, file = cs_output, sep = '\t', row.names = T, col.names = T, dec = ',')
+  write.table(cumscor_out, file = cs_output, sep = '\t', row.names = T, col.names = T, dec = ',')  
   
-  #Visualization:
-  #Was the problem with concatenating by merge - row multiplication inspite of equilibrity of dfs' nrows and restriction of the concatinating at all
-  #CumScore_w_tax2<-merge(cumscor_out, cumscor[,1:8], by.x ='ID' , by.y = 'ID_seq', all.x = T)
   names(cumscor)[2]<-'ID'
   CumScore_w_tax2<-data.frame(cumscor[,1:8], cumscor_out)
   
   CumScore_w_tax2<-CumScore_w_tax2[,c(1,2,4:7, 3, 11:ncol(CumScore_w_tax2))]
-  CumScore_w_tax_long<-CumScore_w_tax2 %>% pivot_longer(cols = -c('ID_sp','ID','kingdom','phylum','class','order', 'species'), names_to = 'position', values_to = 'score') %>%
-    mutate(position = as.numeric(sub('X|V','', position))) 
-  
-  ggplot(CumScore_w_tax_long,mapping = aes(x = position, y = paste(class, order, ID), fill = score > 0)) + geom_tile() + theme_bw() +
-    scale_fill_manual(values = c('TRUE' = 'red', 'FALSE' = 'darkblue'))
-  
-  
-  #Summary statistics across taxons:
-  CumScore_summary <- CumScore_w_tax2[,1:7]
-  CumScore_summary$PA <- apply(CumScore_w_tax2[,8:ncol(CumScore_w_tax2)], 1, function(x)sum(x,na.rm = T)>0)
-  
-  
-  PA_kingdom <- CumScore_summary %>% group_by(kingdom) %>% 
-    summarise(N_kingdom = length(PA), R_kingdom = round(sum(PA)/length(PA),2))
-  PA_phylum <- CumScore_summary %>% group_by(phylum) %>% 
-    summarise(N_phylum = length(PA), R_phylum = round(sum(PA)/length(PA),2))
-  PA_class <- CumScore_summary %>% group_by(class) %>% 
-    summarise(N_class = length(PA), R_class = round(sum(PA)/length(PA),2))
-  PA_order <- CumScore_summary %>% group_by(order) %>% 
-    summarise(N_order = length(PA), R_order = round(sum(PA)/length(PA),2))
-  
-  PA_all <- merge(CumScore_summary, PA_kingdom)
-  PA_all <- merge(PA_all, PA_phylum)
-  PA_all <- merge(PA_all, PA_class)
-  PA_all <- merge(PA_all, PA_order)
-  
-  PA_all_output <- distinct(PA_all[,c('kingdom', 'N_kingdom', 'R_kingdom',
-                                      'phylum','N_phylum', 'R_phylum',
-                                      'class','N_class', 'R_class',
-                                      'order','N_order', 'R_order')])
-  PA_all_output <- PA_all_output[order(PA_all_output$kingdom, 
-                                       PA_all_output$phylum, 
-                                       PA_all_output$class, 
-                                       PA_all_output$order),]
-  write.table(PA_all_output, file = PA_output, quote = F, row.names = F, dec = ',', sep = '\t')
-  
-  
   
   #AA_statistics with amyloidogenicy:
   ali_new<-readAAStringSet(alignment, format ='fasta')
@@ -272,10 +222,6 @@ CS_subset<-function(alignment =dir(pattern  ='ali_noname_R.fa'), cumscore = dir(
   gaps_c<-consensus_m['-',]
   consensus_m['-',][consensus_m['-',]==1]<-NA
   AA_cons <- apply(X = as.data.frame(consensus_m), MARGIN = 2, FUN = max)
-  #hydrophilic<-toupper(c('n','d', 'q', 'e', 'k', 'r'))
-  #hydrophobic<-toupper(c('i', 'v', 'l', 'f', 'c', 'm', 'a', 'w'))
-  #neutral<- toupper(c('g', 't', 's', 'y', 'p', 'h'))
-  #заменить группы и из полярных оставить только NQ
   polar_amyl_aa <- c('N', 'Q')
   aromatic_aa <- c('F', 'W', 'Y')
   hydrophobic_aa <-c('A','L', 'M','I','V')
@@ -304,29 +250,21 @@ CS_subset<-function(alignment =dir(pattern  ='ali_noname_R.fa'), cumscore = dir(
   #Output table with all stat by each position:
   write.table(domain_stat, row.names=F, col.names = T, sep='\t', file = summary)
   write.table(domain_stat_wNA, col.names = T, row.names = F, sep = '\t', file = summary_wNA)
+  #Visualization of parameteres counted for alignment positions excluding NAs:
   ggplot(domain_stat,mapping = aes(x = position, y = name, fill = value)) + geom_tile(height=.9) + 
     scale_fill_viridis_c(name = "",limits = c(0.0,1.0),oob=squish) +  theme_bw()
   ggsave(filename = aa_stat,device = 'pdf', width = 20, height = 4, units = 'cm')
   
+  #Visualization of parameteres counted for alignment positions including NAs:
   ggplot(domain_stat_wNA,mapping = aes(x = position, y = name, fill = value)) + geom_tile(height=.9) + 
     scale_fill_viridis_c(name = "",limits = c(0.0,1.0),oob=squish) +  theme_bw()
   ggsave(filename = aa_stat_wNA,device = 'pdf', width = 20, height = 4, units = 'cm')
-
+  
+  #Visualization of amyloidogenicy comparison for alignment positions excluding and including NAs and gaps:
   ggplot(comparison_cs,mapping = aes(x = position, y = name, fill = value)) + geom_tile(height=.9) + 
     scale_fill_viridis_c(name = "",limits = c(0.0,1.0),oob=squish) +  theme_bw()
   ggsave(filename = cs_stat_comp,device = 'pdf', width = 20, height = 4, units = 'cm')
   
-  
-  #Summary table with aa profile and cum. score stat:
-  #ali_df<-data.frame(names = ali_new@unmasked@ranges@NAMES, str_split_fixed(paste(ali_new),'',nchar(paste(ali_new))))
-  #ali_df<-ali_df[order(match(ali_df$name,CumScore_w_tax2$ID)),]
-  #names(ali_df)[1]<-'ID'
-  #ali_df_w_tax<-merge(ali_df, CumScore_w_tax2[,1:7], by.x = 'ID', all.x = T, sort = F)
-  #names(CumScore_w_tax2)[8:ncol(CumScore_w_tax2)]<-gsub('V','X', names(CumScore_w_tax2)[8:ncol(CumScore_w_tax2)])
-  #united<-rbind(ali_df_w_tax, CumScore_w_tax2)
-  #united<-united[order(united$ID),]
-  
-  #write.table(united, file = cs_aa_out, sep = '\t', row.names = F, col.names = T, dec = ',')
 }
 
 #The MAIN-function, including all three previous. No arguments should be told. It is launched in the directory for each protein separetly. 
@@ -341,7 +279,6 @@ MAIN_fun<-function(x = dir(x, all.files = T), ..deparseOpts){
     stat_aa = paste('ali_stat',i,'.tsv', sep = '')
     plot_aa = paste('ali_aa_stat', i, '.pdf', sep = '')
     cs_out = paste('CS_subset', i, '.tsv', sep = '')
-    PA_out = paste('Report_table', i, '.tsv', sep = '')
     aa_stplot = paste('AA_plot', i, '.pdf', sep= '')
     aa_stplot_na = paste('AA_plot_wNA',i,'.pdf',sep='')
     cs_comp_plot = paste('CS_comp_plot',i,'.pdf',sep='')
@@ -349,7 +286,7 @@ MAIN_fun<-function(x = dir(x, all.files = T), ..deparseOpts){
     sumNA = paste('Summary_wNA', i, '.tsv', sep = '')
     muscle_refine(outputpath_noname = ali, filtered_noname = filt)
     aa_stat(fasta =ali, stat = stat_aa, plot = plot_aa )
-    CS_subset(alignment = ali, cs_output = cs_out, PA_output = PA_out, aa_stat = aa_stplot,aa_stat_wNA=aa_stplot_na, cs_stat_comp = cs_comp_plot, summary = sum_st, summary_wNA = sumNA)
+    CS_subset(alignment = ali, cs_output = cs_out, aa_stat = aa_stplot, aa_stat_wNA=aa_stplot_na, cs_stat_comp = cs_comp_plot, summary = sum_st, summary_wNA = sumNA)
     i = i+1  
   }
 }
