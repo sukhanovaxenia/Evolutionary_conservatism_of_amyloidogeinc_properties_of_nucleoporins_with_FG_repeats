f617523f8e842b15013da7601528bc16887fd1b7
sukhanovaxenia <60236068+sukhanovaxenia@users.noreply.github.com>
2021-05-28 21:58:08 +0300

    Update get_ID.R

--- scripts/get_ID.R
+++ scripts/get_ID.R
@@ -1,19 +1,18 @@
-#Для получения списка ID:
-#library(dplyr)
-#library(plyr)
-#library(tidyr)
-#library(Biostrings)
+#For extracting IDs of taxonomic groups:
+library(dplyr)
+library(plyr)
+library(tidyr)
+library(Biostrings)
+
 get_ID<-function(input, output=out){
-#загрузка исходного инпута с базы EggNOG
-  ##input - путь к файлу с ортологами с EggNOG в формате .fa
-  ##output - пусть к файлу со списком ID
+  ##input - path to the input file *_opisto_ortho.fa from EggNOG
+  ##output - path to the file with taxonomy IDs
 out='taxid.txt'
 fastaFile <- readAAStringSet(file=input)
 df <- data.frame(seq_name=names(fastaFile))
 df$seq_name<-as.character(df$seq_name)
 df<-separate(df, seq_name, c('seq_ID', 'seq_name'),sep='[.]', extra='merge',fill='right')
 df<-df[order(df$seq_ID),]
-
-#Выгружаем в отдельный файл список ID
+#Export file with taxonomy IDs:
 write.table(df$seq_ID, file=out, quote=F, row.names=F, col.names=F)
 }
